6ef2fe5cf55f92d4be7519b257b5e9e913a08865
CurtTimson <curt@live.co.uk>
2017-02-12 17:58:45 +0000

    Create configuration file and get foursquare results from API

---
+++ script/config.js
@@ -0,0 +1,7 @@
+var config = {
+  foursquareApi:{
+    url: "https://api.foursquare.com/v2",
+    clientId: "1HFQTJEFGHY4FTMRIV0QKW3SGKYBYUYKBQTIHOMDHU3JFSGV",
+    clientSecret: "5X3UDJO32XXATVZCISWXJ1R24VTDSY4W0O1PITSUP4VASP4O"
+  }
+}
--- script/fsApp.js
+++ script/fsApp.js
@@ -1,7 +1,31 @@
 'use strict';
 
-var fsApp = angular.module("fsApp", [])
+var fsApp = angular.module("fsApp", ['ngRoute'])
 
-.controller('mainController', function() {
+.controller('mainController', function($scope, $route, $http) {
+
+  $scope.submitSearch = function(){
+
+    if ($scope.searchText){
+
+      var searchUrl = config.foursquareApi.url
+                      + "/venues/search?v=20161016&intent=checkin"
+                      + "&client_id=" + config.foursquareApi.clientId
+                      + "&client_secret=" + config.foursquareApi.clientSecret
+                      + "&near=" + $scope.searchText;
+
+
+      $http.get(searchUrl).then(function(response){
+          console.log(response); //TODO: Remove
+
+
+
+        }, function(){
+          console.log("error");
+        });
+
+      }
+
+  };
 
 });
